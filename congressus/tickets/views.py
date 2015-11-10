6d8b755746ad841f64e98ca4f5e9389189143cd3
Daniel Garcia Moreno <danigm@wadobo.com>
2015-11-10 16:51:56 +0100

    TPV lang to english and payment redirection help text

--- congressus/tickets/views.py
+++ congressus/tickets/views.py
@@ -94,6 +94,7 @@ class Payment(TemplateView):
         data["DS_MERCHANT_TRANSACTIONTYPE"] = ttype
         data["DS_MERCHANT_TERMINAL"] = terminal
         data["DS_MERCHANT_MERCHANTURL"] = url
+        data["DS_MERCHANT_CONSUMERLANGUAGE"] = '002'
         data["DS_MERCHANT_URLOK"] = settings.SITE_URL + '/ticket/%s/thanks/' % tk.order
         data["DS_MERCHANT_URLKO"] = settings.SITE_URL + '/ticket/%s/payment/?error=1' % tk.order
 
