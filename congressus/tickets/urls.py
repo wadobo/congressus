9e145e1a6143f8287cad8b9ed9aa9843c1c27f8c
Daniel Garcia Moreno <danigm@wadobo.com>
2016-05-20 16:38:36 +0200

    Added trailing slash to urls, django adds it if it isn't there

--- congressus/tickets/urls.py
+++ congressus/tickets/urls.py
@@ -3,15 +3,15 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^event/(?P<ev>[\w-]+)$', views.event, name='event'),
+    url(r'^event/(?P<ev>[\w-]+)/$', views.event, name='event'),
 
-    url(r'^event/(?P<ev>[\w-]+)/(?P<space>[\w-]+)/(?P<session>[\w-]+)/register', views.register, name='register'),
-    url(r'^ticket/(?P<order>[\w-]+)/payment', views.payment, name='payment'),
-    url(r'^ticket/(?P<order>[\w-]+)/thanks', views.thanks, name='thanks'),
-    url(r'^ticket/confirm', views.confirm, name='confirm'),
+    url(r'^event/(?P<ev>[\w-]+)/(?P<space>[\w-]+)/(?P<session>[\w-]+)/register/$', views.register, name='register'),
+    url(r'^ticket/(?P<order>[\w-]+)/payment/$', views.payment, name='payment'),
+    url(r'^ticket/(?P<order>[\w-]+)/thanks/$', views.thanks, name='thanks'),
+    url(r'^ticket/confirm/$', views.confirm, name='confirm'),
 
-    url(r'^(?P<ev>[\w-]+)$', views.multipurchase, name='multipurchase'),
+    url(r'^(?P<ev>[\w-]+)/$', views.multipurchase, name='multipurchase'),
 
-    url(r'^seats/view/(?P<map>\d+)$', views.seats, name='seats'),
+    url(r'^seats/view/(?P<map>\d+)/$', views.seats, name='seats'),
 ]
 
