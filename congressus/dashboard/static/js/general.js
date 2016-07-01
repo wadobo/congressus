865e0f577928ec3933a81081c026531b69d5ce8d
Victor Ramirez <virako@wadobo.com>
2016-07-01 03:42:47 +0200

    Added line chart updates in realtime with websockets.

--- congressus/dashboard/static/js/general.js
+++ congressus/dashboard/static/js/general.js
@@ -38,22 +38,60 @@ function fill_charts() {
     });
 }
 
-function update_charts(chart, dataset_label, label, amount) {
+function randColor() {
+    res = "rgba(";
+    res += String(Math.random()*255|0) + ","
+    res += String(Math.random()*255|0) + ","
+    res += String(Math.random()*255|0) + ","
+    res += "1)";
+    return res;
+}
+
+function update_line_charts(chart, dataset_label, label, amount=1) {
     main_dataset = 0
     n = 0
-    ndataset = null;
+    extra_dataset = null;
     for (var dat in chart.data.datasets) {
         if (chart.data.datasets[dat].label == dataset_label) {
-            ndataset = n;
+            extra_dataset = n;
             break;
         }
         n++;
     }
     index = chart.data.labels.indexOf(label);
+    new_index = false;
+    if (index === -1) {
+        chart.data.labels.push(label);
+        index = chart.data.labels.length - 1;
+        new_index = true;
+    }
 
-    chart.data.datasets[main_dataset].data[index] += amount;
-    if (ndataset !== null) {
-        chart.data.datasets[ndataset].data[index] += amount;
+    if (new_index) {
+        chart.data.datasets[main_dataset].data.push(amount);
+        if (extra_dataset !== null) {
+            chart.data.datasets[extra_dataset].data.push(amount);
+        }
+    } else {
+        chart.data.datasets[main_dataset].data[index] += amount;
+        if (extra_dataset !== null) {
+            chart.data.datasets[extra_dataset].data[index] += amount;
+        }
+    }
+    if (extra_dataset === null) { // Push new dataset
+        len_data = chart.data.datasets[main_dataset].data.length;
+        new_data = new Array(len_data).fill(0);
+        new_data[len_data - 1] = 1;
+        chart.data.datasets.push({
+            label: dataset_label,
+            fill: false,
+            lineTension: 0,
+            borderColor: randColor(),
+            borderWidth: 1,
+            pointBorderWidth: 1,
+            pointRadius: 1,
+            scaleStartValue: 0,
+            data: new_data,
+        });
     }
     chart.update();
 }
@@ -62,11 +100,11 @@ function websocketCB(ev, data) {
     if (ev === 'add_ac') {
         toT = data.date.indexOf('T');
         date = data.date.substring(0, toT);
-        #update_line_charts(window.chart_access, data.control, date);
+        update_line_charts(window.chart_access, data.control, date);
     } else if (ev === 'add_sale') {
         toT = data.date.indexOf('T');
         date = data.date.substring(0, toT);
-        #update_line_charts(window.chart_sales, data.window, date, data.amount);
+        update_line_charts(window.chart_sales, data.window, date, data.amount);
     }
 }
 
