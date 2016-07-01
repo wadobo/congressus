13223b3003c2a1e7c316c8ea8da3bf964e7d7cb5
Victor Ramirez <virako@wadobo.com>
2016-07-01 11:47:31 +0200

    Added LogAccessControl to admin

--- congressus/access/admin.py
+++ congressus/access/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from .models import AccessControl
+from .models import LogAccessControl
+
 
 
 class AccessControlAdmin(admin.ModelAdmin):
@@ -9,4 +11,11 @@ class AccessControlAdmin(admin.ModelAdmin):
     search_fields = ('name', 'slug', 'event__name')
 
 
+class LogAccessControlAdmin(admin.ModelAdmin):
+    list_display = ('access_control', 'status')
+    readonly_fields = ('date',)
+    list_display = ('access_control', 'status', 'date')
+
+
 admin.site.register(AccessControl, AccessControlAdmin)
+admin.site.register(LogAccessControl, LogAccessControlAdmin)
