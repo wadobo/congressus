a220c5796026c8a9615f3c834e7732f82d43009e
Daniel García Moreno <danigm@wadobo.com>
2017-11-06 14:52:58 +0100

    Improved refill extra sessions command with event opt

--- congressus/tickets/management/commands/refill_extrasessions.py
+++ congressus/tickets/management/commands/refill_extrasessions.py
@@ -1,12 +1,17 @@
 from django.core.management.base import BaseCommand, CommandError
 from tickets.models import Ticket
 from invs.models import Invitation
+from events.models import Event
 
 import multiprocessing
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('event', nargs='?', type=str)
+
     def handle(self, *args, **options):
+        self.event = options.get('event')
 
         p1 = multiprocessing.Process(target=self.refill_tickets)
         p1.start()
@@ -19,6 +24,11 @@ class Command(BaseCommand):
 
     def refill_tickets(self):
         tks = Ticket.objects.filter(confirmed=True, used=False, extra_data__isnull=False)
+
+        if self.event:
+            ev = Event.objects.get(slug=self.event)
+            tks = tks.filter(session__space__event=ev)
+
         c = tks.count()
         for i, tk in enumerate(tks):
             print("tk {}/{} ".format(i, c))
@@ -26,7 +36,13 @@ class Command(BaseCommand):
             tk.save()
 
     def refill_invs(self):
-        invs = Invitation.objects.filter(extra_data__isnull=False)
+        print (self.event)
+        invs = Invitation.objects.filter(type__sessions__orig_sessions__isnull=False)
+
+        if self.event:
+            ev = Event.objects.get(slug=self.event)
+            invs = invs.filter(type__event=ev)
+
         c = invs.count()
         for i, inv in enumerate(invs):
             print("inv {}/{} ".format(i, c))
