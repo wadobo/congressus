982530b7a60e696064da5cbd835cde7f9e370491
Víctor Ramírez de la Corte <virako.9@gmail.com>
2021-09-03 18:53:22 +0200

    Fix forgot params in get_ticket_format

--- congressus/tickets/views.py
+++ congressus/tickets/views.py
@@ -363,7 +363,7 @@ class Thanks(TemplateView):
 
     def post(self, request, order):
         ticket = get_ticket_or_404(order=request.POST.get('ticket'), confirmed=True)
-        response = get_ticket_format(ticket)
+        response = get_ticket_format(ticket, pf=None)
         return response
 
     def get_context_data(self, *args, **kwargs):
